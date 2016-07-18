a813cb302397c309207b3ec19137a334c3e63f81
JackMasterson <jackmasterson5@gmail.com>
2016-07-18 14:27:34 -0400

    Got the jSON correct

--- resumePrac.py
+++ resumePrac.py
@@ -1,197 +1,96 @@
 #! /usr/bin/env python3
 # wordPrac.py - write a Resume-template generator
 
-import docx
+import docx, resumeData
 from docx.shared import Pt
 
 doc = docx.Document()
+data = resumeData.allData
 
 def greeting():
-	print('The following program will generate your provided' +
-		' information in a professional resume template.' +
-		' Please enter all information as you\'d like it to'+
-		' appear.')
+	print('Formatting...')
 
 	nameRole()
 
 def nameRole():
 	
-	print('')
-	print('Let\'s get started. What\'s your professional name?')
-	
-	name = input()
+	name = data['Name']
 	nameHead = doc.add_paragraph(name, 'Title')
 	nameHead.alignment = 0
 	nameHead.runs[0].font.size = Pt(25)
 
-	print('What\'s your general role? ie Web Developer, '+
-		'Writer, Copyeditor, etc.')
-
-	role = input()
+	role = data['Role']
 	nameHead.add_run(' '+role).italic = True
 	doc.paragraphs[0].runs[1].style = "SubtitleChar"
 	
-	print('')
-	print('Next, we\'ll handle contact info.')
-	print('What\'s your email?')
-	email = input()
+	email = data['Email']
 	contactSect = doc.add_paragraph(email, 'NoSpacing')
 	contactSect.alignment = 1
 
-	print('What\'s your phone number?')
-	phone = input()
-
+	phone = data['Phone']
 	contactSect.add_run('  |  ' + phone)
-
 
 	summary()
 
 def summary():
-	print('Write a quick blurb about yourself. What are your '+
-		'strengths, what are you looking to learn?')
 
-	summary = input()
+	summary = data['Summary']
 
 	doc.add_heading('Summary:', 2)
 	summaryInfo = doc.add_paragraph()
 	summaryInfo.add_run(summary)
 	summaryInfo.runs[0].style = 'SubtleEmphasis'
 	
-	workExp()
+	typeIt()
 
-def workExp():
+def typeIt():
 
-	
-	
-	
-	
+	types = ['Work', 'Extra']
+	for typed in types:
+		#print(typed)
+		#print(data[typed])
 
-	
-	while True:
-
-		print('')
-
-		print('Type \'Work\' to add Work Experience. Type '+
-			'\'Extra\' to add Extracurriculars. ' +
-			'Type \'done\' if finished entering both '+
-			'work and extracurricular info.')
-
-		nextSect = input()
-
-		if nextSect == 'Work':
+		if typed == 'Work':
 			workHeader = doc.add_heading('Work Experience:', 2)
 			workHead = doc.add_paragraph()
-			
-
-		elif nextSect == 'Extra':
+		elif typed == 'Extra':
 			workHeader = doc.add_heading('Extracurriculars:', 2)
 			workHead = doc.add_paragraph()
-			
-		elif nextSect == 'done':
-			break
 
-		while True:
-			print('Add your next most recent item name.')
-			lastJob = input()
+		orgs = list(data[typed].keys())
+		#print(orgs)
+		for org in orgs:
+		#	print(org)
+		#	print(data[typed][org])
+			skills = data[typed][org]['skills']
+			dates = data[typed][org]['dates']
+			title = data[typed][org]['title']
+
+			#print(skills)
+			#print(dates)
+			#print(title)
 
 
-			workHead.add_run(lastJob)
-	
+			workHead.add_run(org)
 			workHead.runs[0].bold = True
 
-#			print('What were the dates worked/participated?')
-#			lastDates = input()
-#			workHead.add_run('\t\t\t\t\t' +lastDates).italic = True
-#			workHead.style = 'NoSpacing'
-#
-#			print('What was your title?')
-#			lastTitle = input()
-#			workHead.runs[1].add_break()
-#			workHead.add_run(lastTitle)
-#			workHead.style = 'NoSpacing'
+			workHead.add_run('\t\t\t' + dates).italic = True
+			workHead.style = 'NoSpacing'
 
-#			print('What skills did you learn? Enter one at a time '+
-#				'followed by \'Enter\', then type done to move on.')
-#			skills = []
+			for skill in skills:
+				print(skill)
+				listed = doc.add_paragraph(skill, style='ListBullet2')
+		
+		
+
 			
-#			while True:
-#				skill = input()
-#				if skill == 'done':
-#					break
-#				else:
-#					skills.append(skill)
-#					continue
-
-#			for each in skills: 
-#				listed = doc.add_paragraph(each, style='ListBullet2')
-			
-			print('To add to the same section, type \'add\', '+
-				'followed by the enter key. Otherwise, type \'done\' and hit enter')
-
-			answer = input()
-			if answer == 'add':
-				continue
-			elif answer == 'done':
-				break
-
-#	education()
-
-def education():
-	doc.add_heading('Education:', 2)
-	print('The following will ask about your education.')
-
-	while True:
-		print('')
-		print('Please proceed with the next most recent educational experience: ')
-		print('What\'s the name of the institution? Type \'done\' if finished '+
-				'entering education information.')
-		edu = input()
-		if edu == 'done':
-			break
-		else: 
-			eduHead = doc.add_paragraph()
-			eduHead.add_run(edu)
-			eduHead.style = 'Normal'
-			eduHead.runs[0].bold = True
-
-			print('What are the years attended?')
-			dates = input()
-
-			eduHead.add_run('\t\t\t\t\t\t\t\t' + dates).italic = True
-
-			print('Was it a B.A. or B.S., or something else? If '+
-				'something else, please '+
-				'enter the name of the degree.')
-			deg = input()
-			eduHead.runs[1].add_break()
-			eduHead.add_run(deg)
-			
-			print('What was the degree in?')
-			degIn = input()
-			eduHead.add_run(', ' + degIn)
-			
-			print('If you graduated with any honors (cum laude, '+
-				'magna cum laude, etc.), list them here. If not, hit enter.')
-			honors = input()
-			if honors == '':
-				eduHead.add_run('')
-			else:
-				eduHead.add_run('\t\t\t\t\t\t\t\t\t' + '[' + honors + 
-					']').italic = True
-			
-			print('What was your GPA? If not applicable, just hit enter.')
-			gpa = input()
-			eduHead.runs[4].add_break()
-			if gpa == '':
-				eduHead.add_run()
-			else:
-				eduHead.add_run('GPA: ' + gpa)
 
 
 
-#greeting()
+	
+greeting()
 #nameRole()
-workExp()
+#typeIt()
 #education()
 #contact()
 
--- resumeWrite.py
+++ resumeWrite.py
@@ -1,16 +1,89 @@
 #! /usr/bin/env python3
 #resumeWrite.py - gather data and put into a JSON to be read in resumePrac.py
-resInfo = {'Summary':{}, 'Work':{}, 'Extra':{}, 'Edu':{}}
-print(resInfo)
+import pprint, subprocess
 
+resInfo = {
+		'Name': '',
+		'Role': '',
+		'Summary': '', 
+		'Work':{}, 
+		'Extra':{},
+		'Email': '',
+		'Phone': '', 
+		'Edu':
+			{
+				'School': {},
+				'Dates': {}, 
+				'DegreeType': {}, 
+				'DegreeField': {}, 
+				'GPA': {}, 
+				'Honors': {}
+			}
+	}
+
+print('What is your professional name?')
+name = input()
+resInfo['Name'] = name
+
+print('What is your current role? ie Web Developer, '+
+	'Copywriter, Publicist, etc.')
+role = input()
+resInfo['Role'] = role
+
+print('What is your email?')
+email = input()
+resInfo['Email'] = email
+
+print('What is your phone number?')
+phone = input()
+resInfo['Phone'] = phone
+
+print('Type a two-to-three sentence blurb about what you\'re doing '+
+	' and what you\'re looking for')
+summary = input()
+resInfo['Summary'] = summary
+
+
+print('Now tell us a little about your education.')
+while True:
+	print('What\'s the name of the school you went to?')
+	school = input()
+	resInfo['Edu']['School'] = school
+
+	print('What years did you attend?')
+	datesAtt = input()
+	resInfo['Edu']['Dates'] = datesAtt
+
+	print('Did you obtain a B.S., B.A., or something else? '+
+		'If something else, please enter it.')
+	degType = input()
+	resInfo['Edu']['DegreeType'] = degType
+
+	print('What field was your degree in?')
+	degField = input()
+	resInfo['Edu']['DegreeField'] = degField
+
+	print('What was your GPA?')
+	gpa = input()
+	resInfo['Edu']['GPA'] = gpa
+
+	print('Did you graudate with any honors (ie cum laude, magna cum laude '+
+			', etc.)? If so, enter them.')
+	honors = input()
+	resInfo['Edu']['Honors'] = honors
+
+	print('Enter \'done\' if finished, enter \'add\' to add more schooling.')
+	ans = input()
+	if ans == 'done':
+		break
+	elif ans == 'add':
+		continue
 
 while True:
 	print('')
 
-	print('Type \'Work\' to add Work Experience. Type '+
-		'\'Extra\' to add Extracurriculars. ' +
-		'Type \'done\' if finished entering both '+
-		'work and extracurricular info.')
+	print('Type \'Work\' to add Work Experience, '+
+		'\'Extra\' to add Extracurricular info. Type \'done\' if finished entering all of it.')
 
 	nextSect = input()
 
@@ -20,16 +93,14 @@ while True:
 
 		while True:
 
-			print('Add your next most recent item name, enter '+
-				'\'done\' to quit.')
+			print('Add the name of the company or organization to begin'+
+				'; enter \'done\' to quit.')
 			lastJob = input()
-			
-			
 			
 			if lastJob == 'done':
 				break
 			else:
-				resInfo[nextSect][lastJob] = {'skills' : [], 'dates': []}
+				resInfo[nextSect][lastJob] = {'title': '', 'skills' : [], 'dates': ''}
 
 				skills = resInfo[nextSect][lastJob]['skills']
 				dates = resInfo[nextSect][lastJob]['dates']
@@ -46,25 +117,28 @@ while True:
 						skills.append(skill)
 					continue
 
-				print('Months/Years worked there? done to finish')
+				print('Months/Years worked there? Type done to finish')
 
-				
 				date = input()
 				if date == 'done':
 					break
 				else:
-					dates.append(date)
+					dates = date
+
+				print('What was your title?')
+				title = input()
+				resInfo[nextSect][lastJob]['title'] = title
 				
 
-print('Type a two-to-three sentence blurb about what you\'re doing '+
-	' and what you\'re looking for')
 
-summary = input()
 
-resInfo['Summary'] = [summary]
+print('Writing Data File...')
+resultFile = open('resumeData.py', 'w')
+resultFile.write('allData = '+pprint.pformat(resInfo))
+resultFile.close()
+print('Done writing data file.')
+subprocess.call('./resumePrac.py')
 
-print(resInfo)
-		
 
 
 
