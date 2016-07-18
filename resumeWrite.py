678c3f05525165cc5e5be54715ab87d5a059b49f
JackMasterson <jackmasterson5@gmail.com>
2016-07-18 11:51:05 -0400

    Properly store items down to skills key

--- resumeWrite.py
+++ resumeWrite.py
@@ -20,7 +20,8 @@ while True:
 
 		while True:
 
-			print('Add your next most recent item name.')
+			print('Add your next most recent item name, enter '+
+				'\'done\' to quit.')
 			lastJob = input()
 			
 			
@@ -28,7 +29,22 @@ while True:
 			if lastJob == 'done':
 				break
 			else:
-				resInfo[nextSect][lastJob] = {}
+				resInfo[nextSect][lastJob] = {'skills' : []}
+
+				skills = resInfo[nextSect][lastJob]['skills']
+				
+				print('What did you learn there? Add skills one at '+
+				'a time by hitting enter. Enter \'done\' when finished.')		
+
+				while True:
+
+					skill = input()
+					if skill == 'done':
+						break
+					else:
+						skills.append(skill)
+				print(skills)
+
 				continue
 print(resInfo)
 		
